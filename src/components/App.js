0960773c303051c9f5434eae8d72944c3d57edb5
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 16:57:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Autocomplete from "./Autocomplete";
+
+const suggestions = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Autocomplete suggestions={suggestions}/> 
     </div>
   )
 }
