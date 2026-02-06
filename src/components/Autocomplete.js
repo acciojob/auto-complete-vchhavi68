f843f83e4d9ecece603ce2fa61d97805ad6b417d
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 18:54:58 +0530

    Update Autocomplete.js

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,1 +1,35 @@
 
+import React, { useState, useEffect } from "react";
+
+const Autocomplete = ({ suggestions }) => {
+  const [query, setQuery] = useState("");
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFilteredSuggestions(
+        suggestions.filter((suggestion) =>
+          suggestion.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    }, 500);
+  }, [query, suggestions]);
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Search item</h1>
+      <input type="text" value={query} onChange={handleInputChange} />
+      <ul>
+        {filteredSuggestions.map((suggestion) => (
+          <li key={suggestion}>{suggestion}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Autocomplete;
